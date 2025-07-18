3f6fe9dedca8a4357dd61b2c9ac559323da2f2ce
AmyDighe <amydighe@gmail.com>
2025-07-18 16:50:06 +0100

    adding paired-back single outbreak simulator

---
+++ R/simulate_single_outbreak.R
@@ -0,0 +1,180 @@
+#' Generate a realisation of a single outbreak from a stochastic branching process
+#'
+#' This function species a minimal model for simulating a single outbreak
+#' of an infectious disease following a spillover event.
+#'
+#' This is a minimal set up designed to run quickly and simply and cannot
+#' accommodate dependence of infectees transmissibility on infectors
+#' characteristics (e.g. mutations --> changes in R0)
+#'
+#' This function creates a named list containing the output of simulating a stochastic branching
+#' process.
+#' @param mn_offspring The mean of the offspring distribution (R0)
+#' @param disp_offspring The overdisperion of the offspring distribution. Must be >= 1. When set to 1, equivalent to a Poisson distribution; >1 is a Negative Binomial distribution.
+#' @param max_gen The maximum number of generations of transmission to simulate - default is Inf but with mn_offspring <1 transmission dies out eventually.
+#' @param index_cases The number of initial seeding cases associated with the zoonotic spillover.
+#' @param generation_time_dist The generation time distribution
+#' @param prob_symptomatic The probability that an infected individual develops symptoms
+#' @param infection_to_onset_dist The infection to symptom onset distribution
+#' @param prob_severe The probability that an infection develops severe disease. Note this IS CONDITIONAL on being symptomatic.
+#' @param prob_seek_healthcare_non_severe The probability that a non-severe infection seeks healthcare.
+#' @param prob_seek_healthcare_severe The probability that a severe infection seeks healthcare.
+#' @param onset_to_healthcare_dist The symptom onset to seeking healthcare delay distribution.
+#' @param prob_diagnosis The probability of diagnosis given healthcare sought. Note this is not modeled as being dependent on severity, beyond the difference in probability of seeking care.
+#' @param healthcare_to_diagnosis_dist The distribution of time from healthcare seeking to diagnosis
+#' @param initial_immune The proportion of the population initially immune.
+#'
+#'
+#' @family simulation
+#' @export
+
+  sim_single_outbreak <- function(mn_offspring = 0.90,
+                     disp_offspring = 1,
+                     max_gen = Inf,
+                     index_cases = 1,
+                     generation_time_dist = function(n)
+                       { rgamma(n, shape = 12, rate = 2) },
+                     prob_symptomatic = 0.80,
+                     infection_to_onset_dist = function(n)
+                       { rgamma(n, shape = 6, rate = 2) },
+                     prob_severe = 0.30,
+                     prob_seek_healthcare_non_severe = 0.50,
+                     prob_seek_healthcare_severe = 0.95,
+                     onset_to_healthcare_dist = function(n)
+                       { rgamma(n, shape = 6, rate = 2) },
+                     prob_diagnosis = 0.8,
+                     healthcare_to_diagnosis_dist = function(n)
+                       { rgamma(n, shape = 6, rate = 2) },
+                     initial_immune = 1,
+                      ...){
+  #-----------------------------------------------------------------------------
+  # simulate branching process -------------------------------------------------
+
+    sim_minimal <- function(mn_offspring = 0.90,
+                            disp_offspring = 1,
+                            max_gen = Inf,
+                            index_cases = 1){
+
+      Z <- list()
+      Z[[1]] <- index_cases
+      i <- 1
+
+      if (disp_offspring <= 1.0) {
+
+        while(sum(Z[[i]]) > 0 && i <= max_gen) {
+          Z[[i+1]] <- rpois(n = sum(Z[[i]]),
+                            lambda = mn_offspring)
+          i <- i+1
+        }
+
+      } else {
+
+        while(sum(Z[[i]]) > 0 && i <= gens) {
+
+          Z[[i+1]] <- rnbinom(n = sum(Z[[i]]),
+                              size =  initial_immune *
+                                mn_offspring/(disp_offspring - 1),
+                              mu = mn_offspring)
+          i <- i+1
+
+        }
+      }
+
+      return(Z)
+
+    }
+
+  #-----------------------------------------------------------------------------
+  # reformat output into data frame with 1 row per infected individual ---------
+
+  tmp <- melt(Z)%>%
+    mutate(number = 1, # number for counting generation size
+           infection_generation = L1-1)%>% # -spillover, index case gen 1
+    filter(infection_generation>=1)%>%
+    rename(n_offspring = value)%>%
+    group_by(infection_generation)%>%
+    mutate(node = cumsum(number))%>% # count generation size
+    ungroup()%>%
+    select(-number, -L1)%>%
+    mutate(id = paste0(infection_generation, "-", node))
+
+  ## look up infectors
+  tmp$infector <- NA
+
+  if(dim(tmp)[1]>index_cases){
+    tmp$infector[(index_cases + 1):dim(tmp)[1]] <- tmp%>% uncount(n_offspring) %>%
+      pull(id)
+
+  ## order more intuitively
+  tmp <- tmp %>%
+    relocate(id, infection_generation, n_offspring, infector) %>%
+    select(-node)
+
+  #-----------------------------------------------------------------------------
+  # assign characteristics (symptoms & healthcare seeking) ---------------------
+
+  tmp <- tmp %>%
+    mutate(time_inf_rel = if_else(is.na(infector), 0,
+                                  generation_time_dist(nrow(.))),
+           symptomatic = sample(c(0, 1), nrow(.),
+                                replace = TRUE,
+                                prob = c(1 - prob_symptomatic,
+                                         prob_symptomatic))) %>%
+    mutate(infection_to_onset = if_else(symptomatic == 0,
+                                        NA, infection_to_onset_dist(nrow(.))),
+           severe = if_else(symptomatic == 1,
+                            sample(c(0, 1), nrow(.),
+                                   replace = TRUE,
+                                   prob = c(1-prob_severe, prob_severe)), 0),
+           seek_healthcare = case_when((symptomatic == 1 & severe == 1) ~
+                                         sample(c(0, 1), nrow(.),replace = TRUE,
+                                                prob = c(1 - prob_seek_healthcare_severe,
+                                                         prob_seek_healthcare_severe)),
+                                       (symptomatic == 1 & severe == 0) ~
+                                         sample(c(0, 1), nrow(.),
+                                                replace = TRUE,
+                                                prob = c(1 - prob_seek_healthcare_non_severe,
+                                                         prob_seek_healthcare_non_severe)),
+                                       symptomatic == 0 ~ 0),
+           onset_to_healthcare = if_else(seek_healthcare == 1,
+                                         onset_to_healthcare_dist(nrow(.)), NA),
+           diagnosis = if_else(seek_healthcare == 1,
+                               sample(c(0,1), nrow(.),
+                                      replace = TRUE,
+                                      prob = c(1-prob_diagnosis,
+                                               prob_diagnosis)), 0),
+           healthcare_to_diagnosis = if_else(diagnosis == 1,
+                                             healthcare_to_diagnosis_dist(nrow(.)), NA))
+
+
+  #-----------------------------------------------------------------------------
+  # anchor to time. Note the first spillover defines time = 0 ------------------
+
+  ## time_infection = time_infection of infector + generation time
+
+  infectors <- unique(tmp$infector)
+  tmp$time_infection <- 0
+
+  tmp2 <- vector(mode = "list", length = length(infectors))
+  tmp2[[1]] <- tmp %>% filter(is.na(infector))
+  tmp3 <- bind_rows(tmp2)
+
+  for(i in 2:length(infectors)){
+    tmp2[[i]] <- tmp %>% filter(infector == infectors[i]) %>%
+      mutate(time_infection = tmp3 %>%
+               filter(id == infectors[i]) %>%
+               pull(time_infection) + time_inf_rel)
+    tmp3 <- bind_rows(tmp2)
+  }
+
+  linelist <- tmp3 %>%
+    mutate(time_symptom_onset = time_infection + infection_to_onset) %>%
+    mutate(time_seek_healthcare = time_symptom_onset + onset_to_healthcare)%>%
+    mutate(time_diagnosis = time_seek_healthcare + healthcare_to_diagnosis)
+  } else {
+    linelist = "no h-2-h transmission"
+  }
+
+  return(linelist) }
+
+
