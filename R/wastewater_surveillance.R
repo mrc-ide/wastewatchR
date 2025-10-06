ea72954353dc18b0046c9738be8c7dc9a8f793b4
AmyDighe <amydighe@gmail.com>
2025-10-06 17:31:12 +0100

    adapting to give option to use number of shedders rather than effective shedders (i.e. adding option to implement model 2 from Hewitt et al, as well as existing model 3). Also adding more detailed ouputs ias a list with "tdd" and "sampled_data" so it is possible to interograte probability of detection over time in calculate_wastewater_tdd instead of just outputting the tdd alone.

--- R/wastewater_surveillance.R
+++ R/wastewater_surveillance.R
@@ -2,21 +2,40 @@
 #'
 #' @param day The day being considered
 #' @param infection_counts A time series of the number of individuals infected on each day
+#' @param method The method to use - either "effective_n_shedders" to use the effective number of shedders based on the shedding profile "shedding_dist" (see below) (equivalent to Hewitt Model 3) or "n_shedders" to use the number of shedders (equivalent to Hewitt Model 2) on any one day
 #' @param shedding_dist The distribution of amount shed over time following infection, normalised so the amount of shedding on the day
-#' with the maximum amount is 1.
+#' with the maximum amount is 1. Used if method = "effective_n_shedders"
+#' @param duration duration of shedding, if method = "n_shedders". Should be NA if using shedding_dist
 #' @param shedding_relative_SC2 Amount of shedding that occurs relative to SARS-CoV-2 (required given our pinning to the SC2 data from Hewitt et al)
 #' @family utils
 #' @export
-calculate_shedding <- function(day, infection_counts, shedding_dist, shedding_relative_SC2) {
+calculate_shedding <- function(day, infection_counts, method, shedding_dist, duration, shedding_relative_SC2) {
   # Shift infection counts based on the day difference
-  shedding_contributions <- sapply(1:length(shedding_dist), function(i) {
-    lagged_day <- day - (i - 1)
-    if (lagged_day >= 0) {
-      return(infection_counts$new_infections[lagged_day + 1] * shedding_dist[i] * shedding_relative_SC2)
-    } else {
-      return(0)
-    }
-  })
+
+  if(method == "effective_n_shedders"){
+
+    shedding_contributions <- sapply(1:length(shedding_dist), function(i) {
+      lagged_day <- day - (i - 1)
+      if (lagged_day >= 0) {
+        return(infection_counts$new_infections[lagged_day + 1] * shedding_dist[i] * shedding_relative_SC2)
+      } else {
+        return(0)
+      }
+    })
+  }
+
+  if(method == "n_shedders"){
+
+    shedding_contributions <- sapply(1:duration, function(i) {
+      lagged_day <- day - (i - 1)
+      if (lagged_day >= 0) {
+        return(infection_counts$new_infections[lagged_day + 1] * shedding_relative_SC2)
+      } else {
+        return(0)
+      }
+    })
+  }
+
   # Sum up all the contributions
   return(sum(shedding_contributions))
 }
@@ -30,10 +49,11 @@ calculate_shedding <- function(day, infection_counts, shedding_dist, shedding_re
 #' @param shedding_dist The distribution of amount shed over time following infection, normalised so the amount of shedding on the day
 #' with the maximum amount is 1.
 #' @param shedding_relative_SC2 Amount of shedding that occurs relative to SARS-CoV-2 (required given our pinning to the SC2 data from Hewitt et al)
-#'
+#' @param method The method to use - either "effective_n_shedders" to use the effective number of shedders based on the shedding profile "shedding_dist" (equivalent to Hewitt Model 3) or "n_shedders" to use the number of shedders (equivalent to Hewitt Model 2) on any one day
+#' @param duration duration of shedding, if method = "n_shedders". Should be NA if using shedding_dist
 #' @family post-processing
 #' @export
-generate_number_shedding_time_series <- function(branching_process_output, shedding_dist, shedding_relative_SC2) {
+generate_number_shedding_time_series <- function(branching_process_output, shedding_dist, shedding_relative_SC2, method, duration) {
 
   max_day <- ceiling(max(branching_process_output$time_infection, na.rm = TRUE))
   days <- seq(0, max_day, by = 1)
@@ -42,7 +62,7 @@ generate_number_shedding_time_series <- function(branching_process_output, shedd
   # Apply the function for each day
   shedding_results <- tibble(day = days) |>
     rowwise() |>
-    mutate(shedding_value = calculate_shedding(day, infection_counts, shedding_dist, shedding_relative_SC2))
+    mutate(shedding_value = calculate_shedding(day, infection_counts, method, shedding_dist, duration, shedding_relative_SC2))
   shedding_results <- shedding_results %>%
     left_join(infection_counts, by = "day")
 
@@ -50,6 +70,11 @@ generate_number_shedding_time_series <- function(branching_process_output, shedd
 
 }
 
+#' @param wastewater_number_shedding_time_series Output from generate_number_shedding_time_series
+#' @param sampling_frequency The frequency of wastewate sampling, with 1 being daily, 7 being weekly, 14 being fortnightly
+#' @param sampling_method The method used, either "autosampler", "grab" or "moore_swab"
+#' @param detection_approach Approach to translating shedding into probability of detection, either "threshold", "logistic_curve" or "per_person_probability"
+#' @param detection_params A named list. If detection_approach == "threshold", must contain "threshold_limits" and "population". If detection_approach == "logistic_curve", must contain "logistic_beta_0", "logistic_beta_1", "seed", "limit_of_detection", "population". If detection_approach == "per_person_probability", must contain "per_infection_probability_detection". Additionally, if "sampling_method" == "moore_swab", "detection_params must also contain "duration" in days (>=1).
 #' @export
 calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
                                      sampling_frequency,
@@ -84,8 +109,8 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
   }
 
   ## Checking that duration is speciifed if the user has selected moore_swab as sampling_method
-  if (sampling_method == "more_swab" ){
-    stop("if sampling method is moore_swab,'detection_params$duration' must be a number of days")
+  if (sampling_method == "moore_swab" ){
+    stop("if sampling method is moore_swab,'detection_params$duration' must be a number of days >=1")
   }
 
   ## If sampling method = moore_swab, calculate mean shedding over moore swab window
@@ -101,6 +126,7 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
 
   ## For detection_approach == "threshold", ttd is the first time at which the effective number
   ## of shedding individuals eclipses said threshold
+  ## NOTE - we do not currently output probability of detection at the population level as this approach does not lend itself to this as prob is either 1 or 0
   if (detection_approach == "threshold") {
 
     # Check that the necessary logistic parameters exist
@@ -119,6 +145,8 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
         if (nrow(filtered_data) == 0) NA_real_ else min(filtered_data$day)
       }) %>%
       ungroup()
+
+    sampled_data <- NA # for consistency across detection_approaches
   }
 
   ## For detection_approach == "logistic_curve", the effective number of shedding at each sampling timepoint is converted
@@ -133,7 +161,6 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
     # Filter to the sampling days
     set.seed(detection_params$seed)
     sampled_data <- wastewater_number_shedding_time_series %>%
-      filter(day %% sampling_frequency == 0) %>%
       mutate(
         # Convert 'shedding_value' to a probability of detection via logistic curve
         prob_detect = ifelse(shedding_value < detection_params$limit_of_detection, 0,
@@ -141,8 +168,8 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
                                detection_params$logistic_beta_0 +     # -1.229996 from Hewitt et al Fig 5B for Model 3
                                detection_params$logistic_beta_1 *     # 0.258775 from Hewitt et al Fig 5B for Model 3
                                   (100000 * shedding_value / detection_params$population))), # 100000 = population used by Hewitt et. al.
-        # Draw once from a Bernoulli with this probability
-        detect_draw = rbinom(n = n(), size = 1, prob = prob_detect))
+        sampled = ifelse(day %% sampling_frequency == 0, "Yes", "No"),
+        detect_draw = ifelse(day %% sampling_frequency == 0, rbinom(n = n(), size = 1, prob = prob_detect), 0))# Draw once from a Bernoulli with this probability
 
     # The time-to-detection is the first sampled day at which detect_draw == 1
     detection_day <- sampled_data %>%
@@ -153,7 +180,6 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
 
     # Return as a tibble with a single row
     calculated_wastewater_shedding_ttd <- tibble(wastewater_first_day = detection_day)
-
   }
 
   ## For detection_approach == "per_person_probability", we look at new_infections on each sampling day, do a
@@ -161,6 +187,7 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
   ## The time to detection is the first day that draw > 0.
   ## Note: We do this based on the timing of the infection and so the timing isn't quite right - we would have do
   ## something weird with the shedding dist to fully capture this approach.
+  ## NOTE - we do not currently output probability of detection at the population level as this approach does not lend itself to this
   if (detection_approach == "per_person_probability") {
     # Check that the necessary parameter is present
     if (!all(c("per_infection_probability_detection") %in% names(detection_params))) {
@@ -188,6 +215,7 @@ calculate_wastewater_ttd <- function(wastewater_number_shedding_time_series,
     calculated_wastewater_shedding_ttd <- tibble(wastewater_first_day = detection_day)
   }
 
-  return(calculated_wastewater_shedding_ttd)
+  return(list(ttd = calculated_wastewater_shedding_ttd,
+              sampled_data = sampled_data))
 
 }
